bea66962da7becbad3b5613b0fad6a7c6146d25d
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 11:10:11 +0200

    Add file for task 2, local test OK

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, reponse) {
+  if (err) console.log(err); else console.log('code: ', reponse.statusCode);
+})
