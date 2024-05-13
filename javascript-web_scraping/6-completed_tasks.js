3929731a1aa52e7743f20ec272f1e9c86a580d7b
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 13:09:26 +0200

    Add file for task 6, local test OK

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    for (const task of data) {
+      if (task.completed) {
+        if (completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId]++;
+        } else {
+          completedTasksByUser[task.userId] = 1;
+        }
+      }
+    }
+
+    console.log(completedTasksByUser);
+  }
+});
