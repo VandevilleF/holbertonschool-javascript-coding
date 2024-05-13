c0b7dafa2f072ae8c800a8769c44869a8681c1fe
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 11:15:09 +0200

    Change block of if and else

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@
 const request = require('request');
 
 request(process.argv[2], function (err, reponse) {
-  if (err) console.log(err); else console.log('code: ', reponse.statusCode);
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', reponse.statusCode);
+  }
+});
