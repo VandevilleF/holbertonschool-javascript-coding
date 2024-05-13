d9b61ed0bffbdf60f5c14c6be7966c011c4fd683
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 10:45:49 +0200

    semistandard (no space before ',' and missing space after function)

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2] , 'utf8', function(err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) console.log(err); else console.log(data);
 });
