d6bb05188858e26d7ffa334ef02944eefe90ea50
VandevilleF <flo.vandeville@gmail.com>
2024-05-22 11:25:37 +0200

    Add file for task 1, local test OK

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', function() {
+  const read = process.stdin.read();
+  process.stdout.write(`Your name is: ${read}`);
+  process.stdout.write('This important software is now closing\n');
+});
