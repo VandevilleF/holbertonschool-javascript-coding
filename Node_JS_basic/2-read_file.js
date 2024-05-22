4be5eeee1ad08ed03fc599e15782a4643642156c
VandevilleF <flo.vandeville@gmail.com>
2024-05-22 15:59:32 +0200

    Change syntaxe line 20 and 21 for field

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const read = fs.readFileSync(path, { encoding: 'utf8', flag: 'r'});
+    const read = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
     /* trim() supprime les espace blancs en début et fin de ligne */
     const lines = read.split('\n').filter((line) => line.trim() !== '');
     lines.shift();
-    const students = []
+    const students = [];
 
     lines.forEach((line) => {
       const [firstname, lastname, age, field] = line.split(',');
@@ -15,12 +15,11 @@ function countStudents(path) {
         students[field] = [];
       }
       students[field].push(firstname);
-    })
+    });
     console.log(`Number of students: ${lines.length}`);
-    console.log(`Number of students in CS: ${students['CS'].length}. List: ${students['CS'].join(', ')}`);
-    console.log(`Number of students in SWE: ${students['SWE'].length}. List: ${students['SWE'].join(', ')}`);
-  }
-  catch {
+    console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
+    console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
+  } catch {
     throw new Error('Cannot load the database');
   }
 }
