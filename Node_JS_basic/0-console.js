5ad138c1b575df9d1f9334be02297a975ee2ec9b
VandevilleF <flo.vandeville@gmail.com>
2024-05-22 10:45:06 +0200

    Add comment

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,4 +1,10 @@
+/**
+ *displayMessage - Display message in STDOUT
+ * @param {string} message message to display
+ */
+
 function displayMessage(message) {
   console.log(message);
 }
+
 module.exports = displayMessage;
