efd1cc741fdd72011e013fa127096b460ff628ee
VandevilleF <flo.vandeville@gmail.com>
2024-05-24 10:49:16 +0200

    dfghdf

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 if (require.main === module) {
-  process.stdin.setEncoding('utf8');
-
   console.log('Welcome to Holberton School, what is your name?');
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.stdout.write('This important software is now closing\n');
+  process.stdin.on('data', (input) => {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+    console.log('This important software is now closing');
     process.exit();
   });
 }
