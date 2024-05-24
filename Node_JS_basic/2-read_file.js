4bd032f30a139ea119c4633ca68fbeb0a90ec257
VandevilleF <flo.vandeville@gmail.com>
2024-05-24 10:39:03 +0200

    New code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,24 +1,38 @@
 const fs = require('fs');
 
+function parseDataToObject(data) {
+  const lines = data.split('\n');
+  const result = {};
+
+  for (let i = 1; i < (lines.length - 1); i += 1) {
+    const [firstname, , , field] = lines[i].split(',');
+
+    if (field) {
+      if (!result[field]) { result[field] = []; }
+      result[field].push(firstname);
+    }
+  }
+
+  return result;
+}
+
 function countStudents(path) {
   try {
-    const read = fs.readFileSync(path, 'utf8');
-    const lines = read.split('\n');
-    const students = {};
+    const data = fs.readFileSync(path, 'utf8');
+    const students = parseDataToObject(data);
 
-    for (let i = 1; i < (lines.length - 1); i += 1) {
-      const [firstname, , , field] = lines[i].split(',');
-      if (!students[field]) {
-        students[field] = [];
-      }
-      students[field].push(firstname);
-    }
-    console.log(`Number of students: ${lines.length}`);
-    console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
-    console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
-  } catch {
+    let totalStudents = 0;
+    Object.keys(students).forEach((key) => { totalStudents += students[key].length; });
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    Object.keys(students).forEach((key) => {
+      const count = students[key].length;
+      const list = students[key].join(', ');
+      console.log(`Number of students in ${key}: ${count}. List: ${list}`);
+    });
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
-
 module.exports = countStudents;
