4ce81312a2e002646d105eba8ef03f2aec9b1dba
VandevilleF <flo.vandeville@gmail.com>
2024-05-24 10:36:08 +0200

    Change parse of databases lines

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,21 +1,18 @@
-/* eslint-disable no-unused-vars */
 const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const read = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
-    /* trim() supprime les espace blancs en début et fin de ligne */
-    const lines = read.split('\n').filter((line) => line.trim() !== '');
-    lines.shift();
+    const read = fs.readFileSync(path, 'utf8');
+    const lines = read.split('\n');
     const students = {};
 
-    lines.forEach((line) => {
-      const [firstname, lastname, age, field] = line.split(',');
+    for (let i = 1; i < (lines.length - 1); i += 1) {
+      const [firstname, , , field] = lines[i].split(',');
       if (!students[field]) {
         students[field] = [];
       }
       students[field].push(firstname);
-    });
+    }
     console.log(`Number of students: ${lines.length}`);
     console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
     console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
