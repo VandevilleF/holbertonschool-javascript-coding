70e47e27d59d839c528df573791c34c566cfa35a
VandevilleF <flo.vandeville@gmail.com>
2024-05-24 10:47:29 +0200

    Remove readable and add arrow function

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
-function readStdin() {
+if (require.main === module) {
   process.stdin.setEncoding('utf8');
 
   console.log('Welcome to Holberton School, what is your name?');
-  process.stdin.on('readable', function() {
-    const read = process.stdin.read();
-    process.stdout.write(`Your name is: ${read}`);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.stdout.write('This important software is now closing\n');
     process.exit();
   });
-}
-module.exports = readStdin;
-if (require.main === module) {
-  readStdin();
 }
